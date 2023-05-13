5d6ac93daaf8dac4a9e17da4c159ca5a281fc243
rv85 <ravinder.gugloth@gmail.com>
2023-05-13 10:54:26 +0100

    changes made

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
 provider "aws" {
+    region = "eu-west-2"
+    access_key = "AKIAY3W3NGAUDBPY4DPW"
+    secret_key = "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"
+  
+}
+resource "aws_vpc" "development_vpc" {
+    cidr_block = "10.0.0.0/16"
+  
+}
+resource "aws_subnet" "dev-subnet-1" {
+    vpc_id = aws_vpc.development_vpc.id
+    cidr_block = "10.0.10.0/24"
+    availability_zone = "eu-west-2"
   
 }
