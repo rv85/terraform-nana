8b3a0808ecf7839dbcedf609934b28bb2b5d4fef
rv85 <ravinder.gugloth@gmail.com>
2023-05-13 15:07:29 +0100

    code updated

--- main.tf
+++ main.tf
@@ -2,15 +2,37 @@ provider "aws" {
     region = "eu-west-2"
     access_key = "AKIAY3W3NGAUDBPY4DPW"
     secret_key = "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"
-  
-}
+ }
 resource "aws_vpc" "development_vpc" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = var.vpc_cidr_block
+    tags = {
+      Name="development_vpc"
+    }
   
 }
-resource "aws_subnet" "dev-subnet-1" {
+resource "aws_subnet" "dev_subnet-1" {
     vpc_id = aws_vpc.development_vpc.id
-    cidr_block = "10.0.10.0/24"
-    availability_zone = "eu-west-2"
+    cidr_block = var.subnet-cidr-block
+    availability_zone = "eu-west-2b"
+    tags= {
+
+    Name = "dev-subnet-1"
+    env = "test"
   
 }
+}
+# data "aws_vpc" "existing_vpc" {
+#     default = true
+  
+# }
+
+# resource "aws_subnet" "dev-subnet-2" {
+#     vpc_id = data.aws_vpc.existing_vpc.id
+#     cidr_block = "172.31.48.0/20"
+#     tags = {
+#       Name= "existing_vpc-subnet"
+#       env = "testing"
+#     }
+# }
+
+  
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "subnet-cidr-block" {
+    description = "subnet cidr block"
+  
+}
+variable "vpc_cidr_block" {
+    description = "vpc cidr block"
+  
+}
+variable "instance_type" {
+  
+}
+variable "ami" {
+  
+}
